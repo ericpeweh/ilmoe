9a7e6a9900e17c87df6a753497b4c6e9bef382ff
madhiyono <yonclr97@gmail.com>
2021-12-15 19:43:18 +0700

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx}"],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      "red-pastel": "#FFA5D8",
+      "yellow-pastel": "F8F6CF",
+      "ocean-pastel": "#5784BB",
+      "sky-pastel": "#B6D8F3",
+      "purple-pastel": "#BE9DDF",
+    },
+    extend: {},
+  },
+  plugins: [],
 };
