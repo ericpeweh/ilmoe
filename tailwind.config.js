d8254409f2a86b245e6a0166fef16025c5c057ed
ericpeweh <ericpeweh@gmail.com>
2021-12-15 20:04:42 +0700

    add fonts
    tailwind config declaration,
    font-face import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  theme: {
-    colors: {
-      "red-pastel": "#FFA5D8",
-      "yellow-pastel": "F8F6CF",
-      "ocean-pastel": "#5784BB",
-      "sky-pastel": "#B6D8F3",
-      "purple-pastel": "#BE9DDF",
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx}"],
+	theme: {
+		colors: {
+			"red-pastel": "#FFA5D8",
+			"yellow-pastel": "F8F6CF",
+			"ocean-pastel": "#5784BB",
+			"sky-pastel": "#B6D8F3",
+			"purple-pastel": "#BE9DDF"
+		},
+		fontFamily: {
+			regular: ["cyntho-next-regular", "sans-serif"],
+			bold: ["cyntho-next-bold", "sans-serif"],
+			extrabold: ["cyntho-next-extra-bold", "sans-serif"],
+			thin: ["cyntho-next-thin", "sans-serif"]
+		},
+		extend: {}
+	},
+	plugins: []
 };
